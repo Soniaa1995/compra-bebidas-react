2332bc3908c2cf9129e259f8d29f6f7ebefb7cca
Sonia Moreno <94900792+Soniaa1995@users.noreply.github.com>
2024-08-24 10:49:40 +0200

    Apartado de notificacion con toda su funcionalidad terminada + proyecto terminado

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -2,8 +2,10 @@ import { create } from "zustand";  //lo que crea el state es esa funcion de crea
 import { createRecipesSlice, RecipesSliceType } from "./recipeSlice";
 import { devtools } from "zustand/middleware";
 import { FavoritesSliceType, createFavoriteSlice } from "./favoritesSlice";
+import { NotificationSliceType, createNotificationSlice } from "./notificationSlice";
 
-export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(devtools( (...a) => ({ //...coger una copia de todas las funciones de get, set etc..
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType & NotificationSliceType>()(devtools( (...a) => ({ //...coger una copia de todas las funciones de get, set etc..
     ...createRecipesSlice(...a), // y aqui lo pasamos
-    ...createFavoriteSlice(...a)
+    ...createFavoriteSlice(...a),
+    ...createNotificationSlice(...a)
 })))
